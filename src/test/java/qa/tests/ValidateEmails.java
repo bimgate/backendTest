bc37577ba6ed0d6b19de850c01827163eebf0989
Bratislav Miletic <bimgate@gmail.com>
2019-08-29 10:58:26 +0200

    Update ValidateEmails.java
    changed from test to real user data

--- src/test/java/qa/tests/ValidateEmails.java
+++ src/test/java/qa/tests/ValidateEmails.java
@@ -85,7 +85,7 @@ public class ValidateEmails extends TestBase {
             statusCode(200).extract().body().path("email");
 
             for (int x=0; x<bodyComment.size(); x++) {
-                Boolean testB = new ValidateValue().isValidEmailAddress("fsedf.com");
+                Boolean testB = new ValidateValue().isValidEmailAddress(bodyComment.get(x).toString());
                 softly.assertThat(testB).as("Email: %s from postId %s", bodyComment.get(x), allUserPosts.get(i)).isTrue();
             }
         }
